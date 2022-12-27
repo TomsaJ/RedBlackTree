2e2bc90552c1606113446303485508f3822bc125
Julian Tomsa <julian.tomsa@gmail.com>
2022-12-27 23:15:35 +0100

    update header

--- Rot-Schwarz-Baum/Rot-Schwarz-Baum.cpp
+++ Rot-Schwarz-Baum/Rot-Schwarz-Baum.cpp
@@ -2,10 +2,60 @@
 //
 
 #include <iostream>
+#include "redblacktree.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char** argv) {
+	{
+		// TreeNode<int> tree(5);
+		RedBlackTree<int> tree;
+
+		// Einfuegen:
+		tree.insert(5);
+		tree.insert(4);
+		tree.insert(3);
+		tree.insert(2);
+		tree.insert(7);
+		tree.insert(8);
+		tree.insert(9);
+		tree.insert(10);
+
+		// Ausdrucken:
+		std::cout << "Tree: " << tree << "\n"; // erwartet: 2, 3, 4, <5>, 7, 8, 9, 10,
+
+		// Suchen:
+		std::cout << "Searching 3, found: " << tree.search(3)->key << "\n";
+		std::cout << "Searching 21 (not in list), found: " << (void*)tree.search(21) << "\n"; // Falls element nicht in Liste: nullptr
+
+		// Loeschen:
+		std::cout << "removing 8\n";
+		tree.deleteNode(tree.search(8));
+		std::cout << tree << "\n";
+
+		std::cout << "removing 10\n";
+		tree.deleteNode(tree.search(10));
+		std::cout << tree << "\n";
+
+		// Sonderfall: Wurzel loeschen:
+		std::cout << "removing 5 (root node)\n";
+		tree.deleteNode(tree.search(5));
+		std::cout << tree << "\n";
+
+		// Predecessor testen:
+		std::cout << "removing element before 4 (3)\n";
+		tree.deleteNode(tree.search(4)->predecessor());
+		std::cout << tree << "\n";
+
+		// Successor testen:
+		std::cout << "removing element after 4 (7)\n";
+		tree.deleteNode(tree.search(4)->successor());
+		std::cout << tree << "\n";
+
+		// <<--  Hier wird aufgrund des Scope-Endes automatisch der destruktor von tree aufgerufen
+	}
+
+	std::cout << "deleted tree, no errors occurred\n"; // Falls dieser text NICHT erscheint, dann ist das Programm im Destruktor von tree abgestuerzt
+
+	return 0;
 }
 
 // Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
--- Rot-Schwarz-Baum/redblacktree.cpp
+++ Rot-Schwarz-Baum/redblacktree.cpp
@@ -1,1 +1,2 @@
 #include "redblacktree.h"
+
